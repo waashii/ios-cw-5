aa2aceae88ed2e3247aa91d999e64a4a9951b577
waashii <70151834+waashii@users.noreply.github.com>
2022-03-14 18:57:20 +0300

    Resubmitted class work due to file issues

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,10 +9,97 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+               
+               HStack{
+                   Spacer()
+                   Image("Background")
+                     .resizable()
+                     .scaledToFit()
+                     .frame(width: 110, height: 110, alignment: .center)
+                     .clipShape(Circle())
+   //                Spacer()
+   //                    .frame(width: -1.0)
+   //                    .frame(width: 3.0)
+                   Text("iOS_Student")
+                       .fontWeight(.bold)
+                       .foregroundColor(Color.black)
+                       .multilineTextAlignment(.leading)
+                       .padding()
+                   Spacer()
+       Spacer()
+               Spacer()
+                   Spacer()
+                   Spacer()
+                   Spacer()
+                   Spacer()
+               }
+               Spacer()
+               
+               HStack{
+                   Image("meme1")
+                       .resizable()
+                       .frame(width: 110, height: 110, alignment: .center)
+                   
+                   
+                   Image("meme2")
+                       .resizable()
+                       .frame(width: 110, height: 110, alignment: .center)
+                   
+                   
+                   
+                   Image("meme3")
+                       .resizable()
+                       .frame(width: 110, height: 110, alignment: .center)
+                   
+                   
+               }
+               HStack{
+                   
+                   
+                   Image("meme4")
+                       .resizable()
+                       .frame(width: 110, height: 110, alignment: .center)
+                   
+                   
+                   
+                   Image("meme5")
+                       .resizable()
+                       .frame(width: 110, height: 110, alignment: .center)
+                   
+                   
+                   
+                   Image("meme6")
+                       .resizable()
+                       .frame(width: 110, height: 110, alignment: .center)
+               }
+              
+               Spacer()
+               Spacer()
+
+               Spacer()
+
+               Spacer()
+
+               Spacer()
+               
+               
+               
+               
+   //            Spacer()
+              
+   //                .frame(width:#, height:#, alignment: .center)
+   //                .clipShape(Circle())
+         
+               
+           
+       }
+   }
+
+
+        
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
